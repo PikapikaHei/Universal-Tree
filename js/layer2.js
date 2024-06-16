bad84a6a64e7e2481399799ad23e17312a9fecbc
PikapikaHei <157876191+PikapikaHei@users.noreply.github.com>
2024-06-16 17:11:58 +0800

    Update layer2.js

--- js/layer2.js
+++ js/layer2.js
@@ -1,1 +1,25 @@
-
+addLayer("row1-pos0", {
+    name: "proton", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#FF0000",
+    requires: new Decimal(3), // Can be a function that takes requirement increases into account
+    resource: "proton", // Name of prestige currency
+    baseResource: "quarks", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return true},
+})
