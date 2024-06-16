5af9d9d8cc6338462eaafbedb634619a193e3302
PikapikaHei <157876191+PikapikaHei@users.noreply.github.com>
2024-06-16 17:36:09 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -32,6 +32,33 @@ addLayer("q", {
     }
 })
 
+addLayer("e", {
+    name: "electron", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#0000FF",
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account
+    resource: "electron", // Name of prestige currency
+    baseResource: "matter", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 2, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return true},
+})
+
+
 addLayer("p", {
     name: "proton", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
