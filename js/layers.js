0256be7fae88c0298d86c956ab3edd9fbacf7a8f
PikapikaHei <157876191+PikapikaHei@users.noreply.github.com>
2024-06-16 17:30:13 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
-addLayer("p", {
+addLayer("q", {
     name: "quarks", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "Q", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -6,7 +6,6 @@ addLayer("p", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    branches: ['h'],
     color: "#4E40C8",
     requires: new Decimal(1), // Can be a function that takes requirement increases into account
     resource: "quarks", // Name of prestige currency
@@ -33,7 +32,7 @@ addLayer("p", {
     }
 })
 
-addLayer("row1-pos0", {
+addLayer("p", {
     name: "proton", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -42,6 +41,7 @@ addLayer("row1-pos0", {
 		points: new Decimal(0),
     }},
     color: "#FF0000",
+    branches: [q],
     requires: new Decimal(3), // Can be a function that takes requirement increases into account
     resource: "proton", // Name of prestige currency
     baseResource: "quarks", // Name of resource prestige is based on
